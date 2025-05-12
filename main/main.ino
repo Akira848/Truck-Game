90d68d5d4a26616bc8d363478448ad7f1cddc021
akira848 <anaclaramartinsrocha2001@gmail.com>
2025-05-12 19:40:51 -0300

    Atualizações

--- main/main.ino
+++ main/main.ino
@@ -9,7 +9,6 @@ const int btnConfirm = 3;    // Confirmar resposta
 const int btnMath = 4;       // Modo Matemática
 const int btnPortuguese = 5; // Modo Português
 
-
 byte caminhaoCima[8] = {
   B00000,
   B11110,
@@ -46,41 +45,43 @@ bool tempoEsgotado = false;
 // Variáveis para modo matemática
 int respostaCorreta = 0;
 int respostaErrada = 0;
-bool respostaSelecionada = true;
+bool respostaSelecionada = true;  // true = esquerda, false = direita
+bool respostaCorretaEsquerda;     // true = correta está na esquerda
 
 // Variáveis para modo português
 String palavraCorreta;
 String palavraErrada;
-const String palavras3Letras[10] = {"SOL", "PE", "MAR", "LUA", "OVO", "PAO", "UVA", "ZIP", "FIM", "GEL"};
-const String palavras4Letras[10] = {"CASA", "PATO", "LUPA", "BOLA", "TREM", "FOGO", "GATO", "RATO", "OSSO", "VELA"};
-const String palavras5Letras[10] = {"CARRO", "BARCO", "LARVA", "TIGRE", "DRAGO", "CHUVA", "GIRAR", "HORTA", "JOGAR", "LIXO"};
+const String palavras3Letras[10] = {"ASA","GOL","MAR","SIM","OVO","MAE","UVA","NAO","FIM","GEL"};
+const String palavras3LetrasErradas[10] = {"AZA","GOU","NAR","SIN","OVU","MAI","UWA","NAU","FIN","GEU"};
+const String palavras4Letras[10] = {"CASA","BICO","MEIA","BOLO","TREM","BIFE","GELO","AZUL","OSSO","LAÇO"};
+const String palavras4LetrasErradas[10] = {"CAZA","BICU","NEIA","BOLU","TREN","BIFI","JELO","ASUL","OSCO","LASO"};
+const String palavras5Letras[10] = {"VOLTA","CLIMA","PRIMO","TIGRE","ACIMA","MILHO","CILIO","PLANO","PRAGA","TALCO"};
+const String palavras5LetrasErradas[10] = {"VOUTA","CRIMA","PRIMU","TIRGE","ASIMA","MILIO","CILHO","PRANO","PLAGA","TAUCO"};
+
+// Vetores de controle para palavras usadas
+bool palavras3LetrasUsadas[10] = {false};
+bool palavras4LetrasUsadas[10] = {false};
+bool palavras5LetrasUsadas[10] = {false};
 
 void setup() {
-  // Configuração dos botões
   pinMode(btnMove, INPUT_PULLUP);
   pinMode(btnConfirm, INPUT_PULLUP);
   pinMode(btnMath, INPUT_PULLUP);
   pinMode(btnPortuguese, INPUT_PULLUP);
-  
-  // Inicialização do LCD
-  lcd.begin(16,2); // Pode ser desnecessário com I2C
-  lcd.init();      // Correto para inicializar displays I2C
-  lcd.backlight(); // Liga a luz de fundo
- 
-  
-  // Mensagem de boas-vindas
-  mostrarMensagemBoasVindas();
-  
-  // Carregar caracteres customizados
+
+  lcd.init();
+  lcd.backlight();
+
   lcd.createChar(0, caminhaoCima);
   lcd.createChar(1, caminhaoBaixo);
-  
+
+  mostrarMensagemBoasVindas();
   randomSeed(analogRead(0));
 }
 
 void loop() {
   verificarSelecaoModo();
-  
+
   if (!aguardandoResposta && fase <= (currentMode == MODE_MATH ? 4 : 3)) {
     if (rodada <= 10) {
       gerarPergunta();
@@ -91,66 +92,50 @@ void loop() {
       passarParaProximaFase();
     }
   }
-  
+
   if (aguardandoResposta && !tempoEsgotado) {
     atualizarTimer();
     verificarBotoesJogo();
   }
-  
+
   delay(100);
 }
 
-// ===== FUNÇÕES DO JOGO ===== //
-
+// --- Boas-vindas e seleção de modo ---
 void mostrarMensagemBoasVindas() {
-  lcd.createChar(0, caminhaoCima);  // Parte superior do caminhão
-  lcd.createChar(1, caminhaoBaixo);
   lcd.clear();
-  lcd.setCursor(3, 0);
-  /*lcd.print("BEM VINDO");
-  lcd.setCursor(1, 1);
-  lcd.print("CAMINHONEIRO");
-  delay(2000);
-  lcd.clear();*/
   lcd.setCursor(0, 0);
-  lcd.write(byte(0));  // Caminhão superior esquerdo
-  lcd.print("   BEM VINDO  ");
-  lcd.write(byte(0));  // Caminhão superior direito
-  
+  lcd.write(byte(0)); lcd.print("   BEM VINDO  "); lcd.write(byte(0));
   lcd.setCursor(0, 1);
-  lcd.write(byte(1));  // Caminhão inferior esquerdo
-  lcd.print(" CAMINHONEIRO ");
-  lcd.write(byte(1));  
-  
-  delay(5000);
-}
+  lcd.write(byte(1)); lcd.print(" CAMINHONEIRO "); lcd.write(byte(1));
 
-void mostrarTelaInicial() {
+  while (true) {
+    if (digitalRead(btnMath) == LOW) {
+      currentMode = MODE_MATH;
+      break;
+    }
+    if (digitalRead(btnPortuguese) == LOW) {
+      currentMode = MODE_PORTUGUESE;
+      break;
+    }
+    delay(100);
+  }
+
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(currentMode == MODE_MATH ? " Jogo Matematica" : " Jogo Portugues");
-  lcd.setCursor(0, 1);
-  lcd.print(" Pressione START");
-  
-  while(digitalRead(btnMove) == HIGH && digitalRead(btnConfirm) == HIGH);
-  delay(200);
+  lcd.print(currentMode == MODE_MATH ? " Modo Matematica" : " Modo Portugues");
+  delay(1000);
+  reiniciarJogo();
 }
 
 void verificarSelecaoModo() {
   if (digitalRead(btnMath) == LOW) {
     currentMode = MODE_MATH;
     reiniciarJogo();
-    lcd.clear();
-    lcd.print(" Modo Matematica");
-    delay(1000);
   }
-  
   if (digitalRead(btnPortuguese) == LOW) {
     currentMode = MODE_PORTUGUESE;
     reiniciarJogo();
-    lcd.clear();
-    lcd.print(" Modo Portugues");
-    delay(1000);
   }
 }
 
@@ -159,168 +144,151 @@ void reiniciarJogo() {
   rodada = 1;
   pontos = 0;
   aguardandoResposta = false;
-  mostrarTelaInicial();
+
+  // Reseta palavras usadas (Modo Português)
+  memset(palavras3LetrasUsadas, false, sizeof(palavras3LetrasUsadas));
+  memset(palavras4LetrasUsadas, false, sizeof(palavras4LetrasUsadas));
+  memset(palavras5LetrasUsadas, false, sizeof(palavras5LetrasUsadas));
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(currentMode == MODE_MATH ? " Modo Matematica" : " Modo Portugues");
+  delay(1000);
 }
 
-// ===== MODO MATEMÁTICA ===== //
+// --- Geração de perguntas ---
+void gerarPergunta() {
+  if (currentMode == MODE_MATH) gerarPerguntaMatematica();
+  else gerarPerguntaPortugues();
+}
 
 void gerarPerguntaMatematica() {
   lcd.clear();
-  lcd.setCursor(0, 0);
-
-  
   int num1, num2;
-  char operador;
-  
+  char op;
   switch(fase) {
-    case 1: // Soma (60s)
-      num1 = random(5, 50);
-      num2 = random(5, 50);
-      respostaCorreta = num1 + num2;
-      operador = '+';
-      break;
-      
-    case 2: // Subtração (75s)
-      num1 = random(20, 100);
-      num2 = random(5, num1-4);
-      respostaCorreta = num1 - num2;
-      operador = '-';
-      break;
-      
-    case 3: // Multiplicação (90s)
-      num1 = random(2, 12);
-      num2 = random(2, 12);
-      respostaCorreta = num1 * num2;
-      operador = 'x';
-      break;
-      
-    case 4: // Divisão (105s)
-      num2 = random(2, 10);
-      respostaCorreta = random(2, 10);
-      num1 = num2 * respostaCorreta;
-      operador = '/';
-      break;
+    case 1: num1 = random(5, 50); num2 = random(5, 50); respostaCorreta = num1 + num2; op = '+'; break;
+    case 2: num1 = random(20, 100); num2 = random(5, num1-4); respostaCorreta = num1 - num2; op = '-'; break;
+    case 3: num1 = random(2, 10); num2 = random(2, 10); respostaCorreta = num1 * num2; op = 'x'; break;
+    case 4: num2 = random(2, 10); respostaCorreta = random(2, 10); num1 = num2 * respostaCorreta; op = '/'; break;
   }
-  
   respostaErrada = gerarRespostaErrada(respostaCorreta);
-  mostrarContaMatematica(num1, operador, num2);
-  respostaSelecionada = random(0, 2) == 0;
+
+  // Exibe a conta
+  lcd.setCursor(5, 0);
+  lcd.print(num1);
+  lcd.print(op);
+  lcd.print(num2);
+  lcd.print("=?");
+
+  // Posiciona respostas aleatoriamente
+  int posicaoCorreta = random(0, 2); // 0 = esquerda, 1 = direita
+  respostaCorretaEsquerda = (posicaoCorreta == 0);
+
+  if (respostaCorretaEsquerda) {
+    lcd.setCursor(4, 1); lcd.print(respostaCorreta);
+    lcd.setCursor(10, 1); lcd.print(respostaErrada);
+  } else {
+    lcd.setCursor(4, 1); lcd.print(respostaErrada);
+    lcd.setCursor(10, 1); lcd.print(respostaCorreta);
+  }
+
+  respostaSelecionada = true; // Começa selecionando a esquerda
   mostrarSelecao();
 }
 
 int gerarRespostaErrada(int correta) {
-  int erro;
+  int e;
   do {
-    int variacao = random(1, 6);
-    erro = correta + (random(0, 2) ? variacao : -variacao);
-  } while (erro == correta || erro <= 0);
-  return erro;
+    int var = random(1, 6);
+    e = correta + (random(0, 2) ? var : -var);
+  } while(e <= 0 || e == correta);
+  return e;
 }
-
-void mostrarContaMatematica(int num1, char operador, int num2) {
-  lcd.setCursor(9,0);
-  lcd.print(num1);
-  lcd.print(operador);
-  lcd.print(num2);
-  lcd.print("=?");
-}
-
-// ===== MODO PORTUGUÊS ===== //
 
 void gerarPerguntaPortugues() {
   lcd.clear();
-  lcd.setCursor(0, 0);
-  
-  int index;
-  switch(fase) {
-    case 1: // 3 letras (45s)
-      index = random(0, 10);
-      palavraCorreta = palavras3Letras[index];
-      palavraErrada = gerarPalavraErrada(palavraCorreta, 3);
-      break;
-      
-    case 2: // 4 letras (60s)
-      index = random(0, 10);
-      palavraCorreta = palavras4Letras[index];
-      palavraErrada = gerarPalavraErrada(palavraCorreta, 4);
-      break;
-      
-    case 3: // 5 letras (75s)
-      index = random(0, 10);
-      palavraCorreta = palavras5Letras[index];
-      palavraErrada = gerarPalavraErrada(palavraCorreta, 5);
-      break;
+  int i = random(0, 10);
+  bool palavraUsada = true;
+
+  while (palavraUsada) {
+    palavraUsada = false;
+    i = random(0, 10);
+
+    switch(fase) {
+      case 1:
+        if (!palavras3LetrasUsadas[i]) {
+          palavraCorreta = palavras3Letras[i];
+          palavraErrada = palavras3LetrasErradas[i];
+          palavras3LetrasUsadas[i] = true;
+          palavraUsada = false;
+        } else {
+          palavraUsada = true;
+        }
+        break;
+      case 2:
+        if (!palavras4LetrasUsadas[i]) {
+          palavraCorreta = palavras4Letras[i];
+          palavraErrada = palavras4LetrasErradas[i];
+          palavras4LetrasUsadas[i] = true;
+          palavraUsada = false;
+        } else {
+          palavraUsada = true;
+        }
+        break;
+      case 3:
+        if (!palavras5LetrasUsadas[i]) {
+          palavraCorreta = palavras5Letras[i];
+          palavraErrada = palavras5LetrasErradas[i];
+          palavras5LetrasUsadas[i] = true;
+          palavraUsada = false;
+        } else {
+          palavraUsada = true;
+        }
+        break;
+    }
   }
-  
-  mostrarPalavras();
-  respostaSelecionada = random(0, 2) == 0;
+
+  lcd.setCursor(3, 0);
+  lcd.print("QUAL A CERTA?");
+
+  // Posiciona palavras aleatoriamente
+  int posicaoCorreta = random(0, 2);
+  respostaCorretaEsquerda = (posicaoCorreta == 0);
+
+  if (respostaCorretaEsquerda) {
+    lcd.setCursor(4, 1); lcd.print(palavraCorreta);
+    lcd.setCursor(10, 1); lcd.print(palavraErrada);
+  } else {
+    lcd.setCursor(4, 1); lcd.print(palavraErrada);
+    lcd.setCursor(10, 1); lcd.print(palavraCorreta);
+  }
+
+  respostaSelecionada = true; // Começa selecionando a esquerda
   mostrarSelecao();
 }
 
-String gerarPalavraErrada(String correta, int tamanho) {
-  String errada;
-  do {
-    errada = "";
-    for (int i = 0; i < tamanho; i++) {
-      errada += (char)random('A', 'Z' + 1);
-    }
-  } while (errada == correta);
-  return errada;
-}
-
-void mostrarPalavras() {
-  lcd.setCursor(2, 1);
-  lcd.print(" ");
-  lcd.print(respostaSelecionada ? palavraCorreta : palavraErrada);
-  lcd.setCursor(8, 1);
-  lcd.print(" ");
-  lcd.print(respostaSelecionada ? palavraErrada : palavraCorreta);
-}
-
-// ===== FUNÇÕES COMUNS ===== //
-
-void gerarPergunta() {
-  if (currentMode == MODE_MATH) {
-    gerarPerguntaMatematica();
-  } else {
-    gerarPerguntaPortugues();
-  }
-}
-
+// --- Seleção do caminhão ---
 void mostrarSelecao() {
-  if (currentMode == MODE_MATH) {
-    lcd.setCursor(2, 1);
-    lcd.print(respostaSelecionada ? respostaCorreta : respostaErrada);
-    lcd.setCursor(10, 1);
-    lcd.print(respostaSelecionada ? respostaErrada : respostaCorreta);
-  }
-  
-  lcd.setCursor(respostaSelecionada ? 0 : 8, 1);
-  lcd.write(respostaSelecionada ? 0 : 1);
+  lcd.setCursor(2, 1); lcd.print(' ');
+  lcd.setCursor(8, 1); lcd.print(' ');
+  int col = respostaSelecionada ? 2 : 8;
+  lcd.setCursor(col, 1);
+  lcd.write(byte(0));
 }
 
+// --- Timer e botões ---
 void atualizarTimer() {
-  unsigned long tempoAtual = millis() - tempoInicioRodada;
-  unsigned long tempoLimite;
+  unsigned long t = millis() - tempoInicioRodada;
+  unsigned long lim = (currentMode == MODE_MATH ? (unsigned long[]){60000, 75000, 90000, 105000}[fase-1] : (unsigned long[]){45000, 60000, 75000}[fase-1]);
   
-  if (currentMode == MODE_MATH) {
-    unsigned long temposMath[4] = {60000, 75000, 90000, 105000};
-    tempoLimite = temposMath[fase - 1];
-  } else {
-    unsigned long temposPortugues[3] = {45000, 60000, 75000};
-    tempoLimite = temposPortugues[fase - 1];
-  }
-
-  if (tempoAtual >= tempoLimite) {
+  if (t >= lim) {
     tempoEsgotado = true;
-    lcd.setCursor(0, 0);
-    lcd.print("0 ");
-    respostaSelecionada = false;
+    lcd.setCursor(0, 0); lcd.print("0 ");
     verificarResposta();
   } else {
     lcd.setCursor(0, 0);
-    lcd.print("");
-    lcd.print((tempoLimite - tempoAtual) / 1000);
+    lcd.print((lim - t) / 1000);
     lcd.print(" ");
   }
 }
@@ -328,13 +296,9 @@ void atualizarTimer() {
 void verificarBotoesJogo() {
   if (digitalRead(btnMove) == LOW) {
     respostaSelecionada = !respostaSelecionada;
-    if (currentMode == MODE_PORTUGUESE) {
-      mostrarPalavras();
-    }
     mostrarSelecao();
     delay(300);
   }
-  
   if (digitalRead(btnConfirm) == LOW) {
     verificarResposta();
     delay(500);
@@ -343,23 +307,22 @@ void verificarBotoesJogo() {
 
 void verificarResposta() {
   aguardandoResposta = false;
-  
   lcd.clear();
   lcd.setCursor(0, 0);
-  
+
+  bool acertou = (respostaSelecionada == respostaCorretaEsquerda);
+
   if (tempoEsgotado) {
     lcd.print(" TEMPO ESGOTADO!");
-  } else if (respostaSelecionada) {
+  } else if (acertou) {
     lcd.print("Resposta Correta");
     pontos++;
   } else {
     lcd.print("Resposta Errada");
   }
-  
-  lcd.setCursor(5, 1);
-  lcd.print("Pts:");
-  lcd.print(pontos);
-  
+
+  lcd.setCursor(4, 1);
+  lcd.print("PONTOS:"); lcd.print(pontos);
   rodada++;
   delay(2000);
 }
@@ -367,35 +330,31 @@ void verificarResposta() {
 void passarParaProximaFase() {
   fase++;
   rodada = 1;
-  
-  int maxFases = (currentMode == MODE_MATH) ? 4 : 3;
-  
-  if (fase > maxFases) {
-    mostrarResultadoFinal();
+  int maxF = (currentMode == MODE_MATH ? 4 : 3);
+
+  if (fase > maxF) {
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print("  Fim do Jogo!");
+    lcd.setCursor(0, 1); lcd.print(" Acertos: "); lcd.print(pontos); lcd.print("/"); lcd.print(maxF * 10);
+    
+    while (digitalRead(btnConfirm) == HIGH) {
+      delay(100);
+    }
+    
+    currentMode = MODE_MATH;
+    fase = 1;
+    aguardandoResposta = false;
+    mostrarMensagemBoasVindas();
+    randomSeed(analogRead(0));
   } else {
     lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(" Proxima Fase: ");
-    lcd.print(fase);
-    lcd.setCursor(0, 1);
-    lcd.print(" Pressione START");
+    lcd.setCursor(0, 0); lcd.print(" Proxima Fase: "); lcd.print(fase);
+    lcd.setCursor(0, 1); lcd.print(" Pressione MOVE");
     
-    while(digitalRead(btnMove) == HIGH);
+    while (digitalRead(btnMove) == HIGH) {
+      delay(100);
+    }
+    
     delay(200);
-  }
-}
-
-void mostrarResultadoFinal() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("  Fim do Jogo!");
-  lcd.setCursor(0, 1);
-  lcd.print(" Acertos: ");
-  lcd.print(pontos);
-  lcd.print("/");
-  lcd.print(currentMode == MODE_MATH ? "40" : "30");
-  
-  while(true) {
-    delay(1000);
   }
 }
